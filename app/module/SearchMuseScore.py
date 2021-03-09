fc6f346834952ca7607a352a537f13d3f4d4e4fd
phantuanminh <phantuanminh2002@gmail.com>
2021-03-10 03:50:52 +0700

    Init Search MuseScore

---
+++ app/module/SearchMuseScore.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+
+
+class SearchMuseScore:
+    def __init__(self):
+        # MuseScore search engine link
+        self._search_link_builder = "https://musescore.com/sheetmusic?text="
+        self._search_name = ""
+
+    def search(self):
+        pass
+
